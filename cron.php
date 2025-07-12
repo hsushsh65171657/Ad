872f420b23f00ae4beeade2461c2410aa3c1987e
hsushsh65171657 <seaftv1@gmail.com>
2025-07-12 23:26:13 +0300

    Delete cron.php

--- cron.php
+++
@@ -1,16 +0,0 @@
-<?php
-require_once 'rolet.php';
-
-$game = loadGame();
-if (!$game || $game['started']) exit;
-
-if (time() >= $game['start_time'] + GAME_DURATION) {
-    $game['started'] = true;
-    saveGame($game);
-    $chat_id = $game['chat_id'];
-
-    // هنا تبدأ اللعبة فعليًا (نخلي placeholder)
-    sendMessage($chat_id, "🎯 اللعبة بدأت! (هنا نعرض العجلة ونبدأ اختيار اللاعبين)");
-
-    // راح نكمل هنا جزء رسم العجلة لاحقًا 👇
-}
