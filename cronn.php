6bf976da5e11a03be95802f6db6b44b946b9c0b0
hsushsh65171657 <seaftv1@gmail.com>
2025-07-13 17:18:47 +0300

    Add files via upload

---
+++ cronn.php
@@ -0,0 +1,9 @@
+<?php
+
+while (true) {
+    // شغّل الملف الرئيسي كل فترة (مثلاً كل 60 ثانية)
+    file_get_contents("https://hrbymybotsh.duckdns.org/mybots/cron.php");
+
+    // انتظر 60 ثانية قبل تكرار التشغيل
+    sleep(60);
+}
